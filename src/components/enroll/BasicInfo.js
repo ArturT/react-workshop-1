d508faf5ca23a054654e1067f2f491ca123d4546
ArturT <artur.trzop@lunarlogic.io>
2016-01-23 13:42:37 +0100

    toggle legend

--- src/components/enroll/BasicInfo.js
+++ src/components/enroll/BasicInfo.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class BasicInfo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: true }
+  }
+
+  toggleContent() {
+    this.setState({ open: !this.state.open })
+  }
+
+  isActive() {
+    if (this.state.open) {
+      return "fields active";
+    } else {
+      return "fields";
+    }
+  }
+
   render() {
     return (
       <fieldset>
-        <legend>Basic Info</legend>
-        <div className="fields active">
+        <legend onClick={this.toggleContent.bind(this)} className="pointer">Basic Info</legend>
+        <div className={this.isActive()}>
           <label htmlFor="name">First Name</label>
           <input type="text" name="name" placeholder="Arien" />
           <label htmlFor="surname">Surname</label>
--- src/components/enroll/MagicalPreferences.js
+++ src/components/enroll/MagicalPreferences.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class MagicalPreferences extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: true }
+  }
+
+  toggleContent() {
+    this.setState({ open: !this.state.open })
+  }
+
+  isActive() {
+    if (this.state.open) {
+      return "fields active";
+    } else {
+      return "fields";
+    }
+  }
+
   render() {
     return (
       <fieldset>
-        <legend>Magical Preferences</legend>
-        <div className="fields active">
+        <legend onClick={this.toggleContent.bind(this)} className="pointer">Magical Preferences</legend>
+        <div className={this.isActive()}>
           <label htmlFor="house">House</label>
           <div className="select-wrapper">
             <select name="house" defaultValue="">
