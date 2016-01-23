f0e35f23a16df3ad912d49ef97bd39d505d6a771
ArturT <artur.trzop@lunarlogic.io>
2016-01-23 13:27:28 +0100

    Extract enroll components

---
+++ src/components/enroll/BasicInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class BasicInfo extends React.Component {
+  render() {
+    return (
+      <fieldset>
+        <legend>Basic Info</legend>
+        <div className="fields active">
+          <label htmlFor="name">First Name</label>
+          <input type="text" name="name" placeholder="Arien" />
+          <label htmlFor="surname">Surname</label>
+          <input type="text" name="surname" placeholder="Doriath" />
+        </div>
+      </fieldset>
+    )
+  }
+}
+
+export default BasicInfo;
---
+++ src/components/enroll/MagicalPreferences.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class MagicalPreferences extends React.Component {
+  render() {
+    return (
+      <fieldset>
+        <legend>Magical Preferences</legend>
+        <div className="fields active">
+          <label htmlFor="house">House</label>
+          <div className="select-wrapper">
+            <select name="house" defaultValue="">
+              <option value="" disabled>-- Our choices show what we truly are --</option>
+              <option value="gryffindor">Gryffindor</option>
+              <option value="slytherin">Slytherin</option>
+              <option value="ravenclaw">Raveclaw</option>
+              <option value="hufflepuff">Hufflepuff</option>
+            </select>
+          </div>
+          <label htmlFor="pet">Pet Companion</label>
+          <div className="select-wrapper">
+            <select name="pet" defaultValue="">
+              <option value="" disabled>-- Choose Pet Wisely --</option>
+              <option value="owl">Owl</option>
+              <option value="cat">Cat</option>
+              <option value="unicorn">Unicorn</option>
+              <option value="toad">Toad</option>
+              <option value="snake">Snake</option>
+            </select>
+          </div>
+        </div>
+      </fieldset>
+    )
+  }
+}
+
+export default MagicalPreferences;
