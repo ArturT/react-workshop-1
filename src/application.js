7e7f51846a566f20d12e30a1d9aa7d92cba47c01
ArturT <artur.trzop@lunarlogic.io>
2016-01-23 10:30:56 +0100

    add layout

--- src/application.js
+++ src/application.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import Main from "./containers/Main"
 import Enroll from "./containers/Enroll"
 import StudentsList from "./containers/StudentLists"
+import Layout from "./containers/Layout"
 
 var app = document.getElementById('app');
 
-ReactDOM.render(<Main />, app);
+//ReactDOM.render(<Main />, app);
 //ReactDOM.render(<Enroll />, app);
 //ReactDOM.render(<StudentsList />, app);
+ReactDOM.render(<Layout />, app);
---
+++ src/containers/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/shared/Header';
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <section className="main" aria-role="main">
+
+        </section>
+      </div>
+    )
+  }
+};
+
+export default Layout;
