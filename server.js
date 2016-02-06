b3674ccf64601e71b094f5592f529ca96596dde8
ArturT <artur.trzop@lunarlogic.io>
2016-02-06 14:41:02 +0100

    render server side the react app (only partial now). Extract history because it doesn’t work on server side

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ import WebpackHotMiddleware from 'webpack-hot-middleware';
 import Express from 'express';
 import http from 'http';
 import path from 'path';
+import { renderToString } from 'react-dom/server'
+import { match, RoutingContext } from 'react-router'
+import React from 'react';
 
+import routes from './src/routes'
 import config from './webpack/development.config.js';
 import defaultConfig from './webpack/default.config.js';
 
@@ -26,9 +30,24 @@ app.use(WebpackHotMiddleware(compiler, {
   path: '/__webpack_hmr'
 }));
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
-});
+//app.get('*', (req,res) => {
+  //res.sendFile(path.join(__dirname + '/views/index.html'));
+//});
+app.use('*', (req, res) => {
+  // Note that req.url here should be the full URL path from
+  // the original request, including the query string.
+  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).send(error.message)
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      res.status(200).send(renderToString(<RoutingContext {...renderProps} />))
+    } else {
+      res.status(404).send('Not found')
+    }
+  })
+})
 
 http.createServer(app).listen(port, ip, (err) => {
   if(err) {
--- src/application.js
+++ src/application.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './routes';
+import history from './history.js'
 
 var app = document.getElementById('app');
 
-ReactDOM.render(routes, app);
+ReactDOM.render((
+  <Router history={history}>
+    {routes}
+  </Router>
+), app);
